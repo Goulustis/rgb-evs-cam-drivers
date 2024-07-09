9d4188700d71a891f96c9afce393805d60b21e6d
UBCYujia <ubcamber@outlook.com>
2024-07-09 16:58:41 -0700

    support deleting files

--- del_failed.py
+++ del_failed.py
@@ -2,6 +2,7 @@ from config import SCENE, RGB_SAVE_DIR, EVS_SAVE_DIR, GREEN, RED, RESET
 import os.path as osp
 import shutil
 import glob
+import os
 
 user_input = input(f"continue with deleting \"{RED}{SCENE}{RESET}\"? (yes/no): {RESET}").lower().strip()
 
@@ -21,6 +22,10 @@ for del_dir in to_del_dirs:
     user_input = input(f"deleting \"{RED}{del_dir}{RESET}\"? (yes/no): {RESET}").lower().strip()
     if user_input != "yes":
         continue
+    
+    try:
+        shutil.rmtree(del_dir)
+    except:
+        os.remove(del_dir)
 
-    shutil.rmtree(del_dir)
     print(f"{GREEN} removed {del_dir} {RESET}")
